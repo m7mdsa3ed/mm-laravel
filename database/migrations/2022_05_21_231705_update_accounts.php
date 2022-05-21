336ea6561e2b009e7c0c6b0ec0d9fbe465ee52fd
M7mDSa3eD <m7md.sa3ed@hotmail.com>
2022-05-22 01:25:04 +0200

    name, user_id unique db inde

---
+++ database/migrations/2022_05_21_231705_update_accounts.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('accounts', function (Blueprint $table) {
+            $table->dropUnique('accounts_name_unique');
+            $table->unique(['name', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('accounts', function (Blueprint $table) {
+            $table->unique('name');
+            $table->dropUnique(['name', 'user_id']);
+        });
+    }
+};
