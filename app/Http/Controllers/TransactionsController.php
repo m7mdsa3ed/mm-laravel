d83c996f55010670e15ba221ce3e0ea4f7d1658f
Mohamed Saeed <m7md.sa3ed@hotmail.com>
2023-03-20 21:08:46 +0200

    show move transactions by default

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -31,7 +31,6 @@ class TransactionsController extends Controller
                 'dates' => [$request->date_from, $request->date_to],
                 'period' => $request->period,
             ])
-            ->when($request->show_move, fn ($query) => $query->withoutGlobalScope('moveTransactions'))
             ->simplePaginate();
 
         $transactions->append('action_type_as_string');
