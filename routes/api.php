c08da3af76770a8dbe97487818b2efcf7b62b85f
Mohamed Saeed <m7md.sa3ed@hotmail.com>
2023-03-29 23:58:51 +0200

    cs fixes

--- routes/api.php
+++ routes/api.php
@@ -13,62 +13,80 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('', function () {
+Route::get('/', function () {
     return [
-        'laravel_version' => app()->version(),
-        'api_version' => 1,
+        'message' => 'Hello World!',
     ];
 });
 
 Route::post('login', 'AuthenticationController@authenticate');
+
 Route::post('register', 'AuthenticationController@register');
 
 Route::get('appInfo', 'GeneralController@appInfo');
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('me', 'AuthenticationController@me');
+
     Route::post('logout', 'AuthenticationController@unauthenticate');
 
     Route::get('stats', 'GeneralController@stats');
+
     Route::get('balance-details', 'GeneralController@getBalanceDetails');
 
     Route::prefix('accounts')->group(function () {
         Route::get('', 'AccountsController@viewAny');
+
         Route::post('', 'AccountsController@save');
+
         Route::get('{account}', 'AccountsController@show');
+
         Route::post('{account}/update', 'AccountsController@save');
+
         Route::post('{account}/delete', 'AccountsController@delete');
     });
 
     Route::prefix('categories')->group(function () {
         Route::get('', 'CategoriesController@viewAny');
+
         Route::post('', 'CategoriesController@save');
+
         Route::post('{category}/update', 'CategoriesController@save');
+
         Route::post('{category}/delete', 'CategoriesController@delete');
     });
 
     Route::prefix('tags')->group(function () {
         Route::get('', 'TagsController@viewAny');
+
         Route::post('', 'TagsController@save');
+
         Route::post('{tag}/update', 'TagsController@save');
+
         Route::post('{tag}/delete', 'TagsController@delete');
     });
 
     Route::prefix('transactions')->group(function () {
         Route::get('', 'TransactionsController@viewAny');
+
         Route::post('', 'TransactionsController@save');
+
         Route::post('{transaction}/update', 'TransactionsController@save');
+
         Route::post('{transaction}/delete', 'TransactionsController@delete');
+
         Route::post('move', 'TransactionsController@moveMoney');
     });
 
     Route::prefix('currencies')->middleware(['role:manager'])->group(function () {
         Route::get('', 'CurrenciesController@viewAny');
+
         Route::post('update/{currency}', 'CurrenciesController@save');
     });
 
     Route::prefix('roles')->middleware(['role:manager'])->group(function () {
         Route::get('', 'RolesController@viewAny')->name('roles.viewAny');
+
         Route::post('syncRoles', 'RolesController@syncRoles');
     });
 });
