6fcf52379eae4b27811772e5ec8dd10316371dfd
Mohamed Saeed <m7md.sa3ed@hotmail.com>
2023-04-07 04:15:43 +0200

    change app storage path

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,8 @@ $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
+$app->useStoragePath(env('APP_STORAGE_PATH', storage_path()));
+
 /*
 |--------------------------------------------------------------------------
 | Bind Important Interfaces
