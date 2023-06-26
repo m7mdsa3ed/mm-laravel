79cd18222b43cf0da55d6f4637cc1fa25b35a7de
Mohamed Saeed <m7md.sa3ed@hotmail.com>
2023-06-26 22:28:58 +0300

    add some attributes to budget modal

--- app/Models/Budget.php
+++ app/Models/Budget.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Enums\BudgetType;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -23,6 +25,12 @@ class Budget extends Model
         'category_id',
     ];
 
+    protected $appends = [
+        'progress',
+        'type_as_string',
+        'exceeded_amount',
+    ];
+
     /** @return BelongsTo */
     public function user(): BelongsTo
     {
@@ -33,5 +41,50 @@ class Budget extends Model
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function progress(): Attribute
+    {
+        return Attribute::make(
+            get: function () {
+                if ($this->getRawOriginal('balance') === null) {
+                    return 0;
+                }
+
+                $progress = round($this->getRawOriginal('balance') / $this->amount * 100, 2);
+
+                return min($progress, 100);
+            }
+        );
+    }
+
+    public function exceededAmount(): Attribute
+    {
+        return Attribute::make(
+            get: function () {
+                if ($this->getRawOriginal('balance') === null) {
+                    return 0;
+                }
+
+                $exceededAmount = $this->getRawOriginal('balance') - $this->amount;
+
+                return max($exceededAmount, 0);
+            }
+        );
+    }
+
+    public function typeAsString(): Attribute
+    {
+        return Attribute::make(
+            get: function () {
+                $type = $this->type;
+
+                try {
+                    return BudgetType::from($type)->name;
+                } catch (\Throwable) {
+                    return null;
+                }
+            }
+        );
     }
 }
