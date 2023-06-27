a6c5596814edf26a091a28ae4bca79cf395f069f
Mohamed Saeed <m7md.sa3ed@hotmail.com>
2023-06-27 13:58:12 +0300

    remove importing transactions for now

--- app/Services/Transactions/Contracts/TransactionsImporter.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Services\Transactions\Contracts;
-
-interface TransactionsImporter
-{
-    public function import(string $filePath);
-}
--- app/Services/Transactions/TransactionService.php
+++ app/Services/Transactions/TransactionService.php
@@ -2,38 +2,9 @@
 
 namespace App\Services\Transactions;
 
-use App\Services\Transactions\Contracts\TransactionsImporter;
-use App\Services\Transactions\Importers\ADIBCSVImporter;
 use App\Traits\HasInstanceGetter;
-use Exception;
 
 class TransactionService
 {
     use HasInstanceGetter;
-
-    /** @throws Exception */
-    public function import(string $type, string $filePath)
-    {
-        $importer = $this->getImporter($type);
-
-        if (!$importer) {
-            throw new Exception('Importer not found');
-        }
-
-        return $importer->import($filePath);
-    }
-
-    private function getImporter(string $type): ?TransactionsImporter
-    {
-        $importer = match ($type) {
-            'adib-csv' => ADIBCSVImporter::class,
-            default => null,
-        };
-
-        if ($importer) {
-            return app($importer);
-        }
-
-        return null;
-    }
 }
