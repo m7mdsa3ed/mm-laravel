54edf5d5be905b3b64c461ab953790d5b90ca81b
Mohamed Saeed <m7md.sa3ed@hotmail.com>
2023-07-15 01:03:15 +0300

    add more details fro summary query

--- app/Queries/MonthBalanceQuery.php
+++ app/Queries/MonthBalanceQuery.php
@@ -14,11 +14,17 @@ class MonthBalanceQuery
             ->join('currencies', 'accounts.currency_id', '=', 'currencies.id')
             ->select(
                 DB::raw('COALESCE(SUM(CASE WHEN action = 1 THEN amount ELSE 0 END), 0) AS in_amount'),
+                DB::raw('COALESCE(SUM(CASE WHEN action = 1 AND action_type = 4 THEN amount ELSE 0 END), 0) AS in_loan_amount'),
+                DB::raw('COALESCE(SUM(CASE WHEN action = 1 AND action_type = 5 THEN amount ELSE 0 END), 0) AS in_debit_amount'),
+                DB::raw('COALESCE(SUM(CASE WHEN action = 1 AND action_type = 6 THEN amount ELSE 0 END), 0) AS in_held_amount'),
                 DB::raw('COALESCE(SUM(CASE WHEN action = 2 THEN amount ELSE 0 END), 0) AS out_amount'),
+                DB::raw('COALESCE(SUM(CASE WHEN action = 2 AND action_type = 4 THEN amount ELSE 0 END), 0) AS out_loan_amount'),
+                DB::raw('COALESCE(SUM(CASE WHEN action = 2 AND action_type = 5 THEN amount ELSE 0 END), 0) AS out_debit_amount'),
+                DB::raw('COALESCE(SUM(CASE WHEN action = 2 AND action_type = 6 THEN amount ELSE 0 END), 0) AS out_held_amount'),
                 'currencies.id as currency_id',
                 'currencies.slug as currency_slug',
             )
-            ->whereNotIn('action_type', [3, 4])
+            ->whereNotIn('action_type', [3])
             ->where('transactions.created_at', '>=', $from)
             ->where('transactions.created_at', '<=', $to)
             ->where('transactions.user_id', '=', $userId)
