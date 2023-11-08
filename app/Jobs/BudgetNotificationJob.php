e6ca0fa32d5e4485bc5019bc76205af80ebb3f9e
Mohamed Saeed <m7md.sa3ed@hotmail.com>
2023-11-08 22:31:09 +0200

    send notifications in background job

---
+++ app/Jobs/BudgetNotificationJob.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\GeneralMessageMail;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+use Kreait\Firebase\Messaging\SendReport as FirebaseMessagingSendReport;
+use Kreait\Laravel\Firebase\Facades\Firebase;
+use Throwable;
+
+class BudgetNotificationJob implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+    /** Create a new job instance. */
+    public function __construct(
+        private readonly User $user,
+        private readonly string $message,
+    ) {
+
+    }
+
+    /** Execute the job. */
+    public function handle(): void
+    {
+        $this->sendNotification($this->user, $this->message);
+    }
+
+    private function sendNotification(mixed $user, string $message): void
+    {
+        $subject = 'Budget almost exceeded';
+
+        Mail::to($user->email)
+            ->send(
+                new GeneralMessageMail(
+                    message: $message,
+                    subject: $subject,
+                )
+            );
+
+        $this->sendFcmNotification($user, $message, $subject);
+    }
+
+    private function sendFcmNotification(User $user, string $message, string $subject): void
+    {
+        $firebaseMessaging = Firebase::messaging();
+
+        $messages = $user->fcmTokens
+            ->pluck('token')
+            ->map(fn ($token) => [
+                'token' => $token,
+                'data' => [
+                    'body' => $message,
+                    'title' => $subject,
+                    'icon' => 'favicon.ico',
+                ],
+            ]);
+
+        try {
+            $results = $firebaseMessaging->sendAll($messages);
+
+            $toBeRemoved = $results
+                ->failures()
+                ->map(function (FirebaseMessagingSendReport $report) {
+                    return $report->target()->value();
+                });
+
+            $user->fcmTokens()
+                ->whereIn('token', $toBeRemoved)
+                ->delete();
+        } catch (Throwable) {
+        }
+    }
+}
--- app/Listeners/CheckBudgetBalanceAfterTransactionSaved.php
+++ app/Listeners/CheckBudgetBalanceAfterTransactionSaved.php
@@ -3,14 +3,9 @@
 namespace App\Listeners;
 
 use App\Enums\ActionEnum;
-use App\Mail\GeneralMessageMail;
+use App\Jobs\BudgetNotificationJob;
 use App\Models\Budget;
-use App\Models\User;
 use App\Queries\BudgetsGetAllQuery;
-use Illuminate\Support\Facades\Mail;
-use Kreait\Firebase\Messaging\SendReport as FirebaseMessagingSendReport;
-use Kreait\Laravel\Firebase\Facades\Firebase;
-use Throwable;
 
 class CheckBudgetBalanceAfterTransactionSaved
 {
@@ -45,7 +40,7 @@ class CheckBudgetBalanceAfterTransactionSaved
         foreach ($budgetsAlmostExceeded as $budget) {
             $message = $this->getNotificationMessage($budget);
 
-            $this->sendNotification($user, $message);
+            dispatch(new BudgetNotificationJob($user, $message));
         }
     }
 
@@ -68,56 +63,6 @@ class CheckBudgetBalanceAfterTransactionSaved
         $budgetPercentage = $budget->percentage;
 
         return "Your budget $budgetName is almost exceeded. You have spent $budgetPercentage% of $budgetAmount.";
-    }
-
-    private function sendNotification(mixed $user, string $message): void
-    {
-        $subject = 'Budget almost exceeded';
-
-        Mail::to($user->email)
-            ->queue(
-                new GeneralMessageMail(
-                    message: $message,
-                    subject: $subject,
-                )
-            );
-
-        $this->sendFcmNotification($user, $message, $subject);
-    }
-
-    private function sendFcmNotification(User $user, string $message, string $subject): void
-    {
-        if (app()->runningUnitTests()) {
-            return;
-        }
-
-        $firebaseMessaging = Firebase::messaging();
-
-        $messages = $user->fcmTokens
-            ->pluck('token')
-            ->map(fn ($token) => [
-                'token' => $token,
-                'data' => [
-                    'body' => $message,
-                    'title' => $subject,
-                    'icon' => 'favicon.ico',
-                ],
-            ]);
-
-        try {
-            $results = $firebaseMessaging->sendAll($messages);
-
-            $toBeRemoved = $results
-                ->failures()
-                ->map(function (FirebaseMessagingSendReport $report) {
-                    return $report->target()->value();
-                });
-
-            $user->fcmTokens()
-                ->whereIn('token', $toBeRemoved)
-                ->delete();
-        } catch (Throwable) {
-        }
     }
 
     private function checkChanges(array $changes): bool
