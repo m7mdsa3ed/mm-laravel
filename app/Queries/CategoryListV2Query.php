b82ab87026581c0881f5c927c7ae0edd8ea1bc83
Mohamed Saeed <m7md.sa3ed@hotmail.com>
2025-01-03 16:34:49 +0200

    update category list to include children balance

--- app/Queries/CategoryListV2Query.php
+++ app/Queries/CategoryListV2Query.php
@@ -9,36 +9,75 @@ class CategoryListV2Query
     public static function get(int $userId): array
     {
         $sql = <<<SQL
-            WITH RECURSIVE CategoryHierarchy AS (
-                SELECT
-                    id AS category_id,
-                    name AS category_name,
-                    parent_id,
-                    name AS path
-                FROM categories
-                WHERE parent_id IS NULL and user_id = $userId
+            WITH RECURSIVE
+                Descendants AS (
+                    SELECT
+                        id,
+                        id AS ancestor_id
+                    FROM
+                        categories
+                    WHERE user_id = $userId
+                    UNION ALL
+                    SELECT
+                        c.id,
+                        d.ancestor_id
+                    FROM
+                        categories c
+                            INNER JOIN
+                        Descendants d
+                        ON
+                            c.parent_id = d.id
+                ),
 
-                UNION ALL
+                Ancestors AS (
+                    SELECT
+                        id,
+                        id AS descendant_id,
+                        0 AS level
+                    FROM
+                        categories
+                    UNION ALL
+                    SELECT
+                        c.parent_id,
+                        a.descendant_id,
+                        a.level + 1
+                    FROM
+                        Ancestors a
+                            INNER JOIN
+                        categories c
+                        ON
+                            a.id = c.id
+                    WHERE
+                        c.parent_id IS NOT NULL
+                ),
 
-                SELECT
-                    c.id AS category_id,
-                    c.name AS category_name,
-                    c.parent_id,
-                    CONCAT(ch.path, ', ', c.name) AS path
-                FROM categories c
-                    INNER JOIN CategoryHierarchy ch ON c.parent_id = ch.category_id
-            )
-
+                AncestorNames AS (
+                    SELECT
+                        a.descendant_id,
+                        GROUP_CONCAT(c.name ORDER BY a.level DESC SEPARATOR ', ') AS ancestor_names
+                    FROM Ancestors a
+                        INNER JOIN categories c ON a.id = c.id
+                    GROUP BY a.descendant_id
+                ),
+                TotalExpenses AS (
+                    SELECT
+                        c.id AS category_id,
+                        COALESCE(SUM(t.amount), 0) AS total_expenses
+                    FROM categories c
+                        LEFT JOIN Descendants d ON c.id = d.ancestor_id
+                        LEFT JOIN transactions t ON t.category_id = d.id AND t.action = 2
+                    GROUP BY c.id
+                )
             SELECT
-                ch.category_id as id,
-                ch.category_name as name,
-                ch.path as parent_names,
-                SUM(t.amount * IF(t.action = 1, 1, -1)) AS balance,
-                COUNT(t.id) AS transactions_count
-            FROM CategoryHierarchy ch
-                     LEFT JOIN transactions t ON t.category_id = ch.category_id
-            GROUP BY ch.category_id, ch.category_name, ch.path
-            ORDER BY ch.category_id;
+                c.id,
+                c.name,
+                IFNULL(an.ancestor_names, c.name) AS parent_names,
+                te.total_expenses as balance,
+                (select count(ts.id) from transactions ts where ts.category_id = c.id) as transactions_count
+            FROM categories c
+                LEFT JOIN AncestorNames an ON c.id = an.descendant_id
+                LEFT JOIN TotalExpenses te ON c.id = te.category_id
+            ORDER BY c.id;
         SQL;
 
         $data = DB::select($sql);
